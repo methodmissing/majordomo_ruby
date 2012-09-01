64d12e9d99991fc35077720c080b9959caf692b2
Lourens Naudé <lourens@methodmissing.com>
2012-09-01 14:17:02 +0100

    Fix the threading test to work with MDP 2

--- test/test_threading.rb
+++ test/test_threading.rb
@@ -1,26 +1,28 @@
 # encoding: utf-8
 
 require File.join(File.dirname(__FILE__), 'helper')
-=begin
+
 class TestThreading < MajordomoTestCase
   def test_send_recv
     threads = []
-    received = false
+    req_received, rep_received = false, false
     threads << Thread.new do
       client = Majordomo::Client.new(BROKER, true)
       client.timeout = 100
-      p client.send("thread_test", "ping")
+      client.send("thread_test", "ping")
+      reply = client.recv("thread_test")
+      rep_received = true if reply
     end
 
     threads << Thread.new do
       worker = Majordomo::Worker.new(BROKER, "thread_test", true)
-      msg = worker.recv
-      worker.recv("pong")
-      received = true if msg
+      request, reply_to = worker.recv
+      req_received = true if request
+      worker.send("pong", reply_to)
     end
 
     threads.map(&:join)
-    assert received
+    assert req_received
+    assert rep_received
   end
 end
-=end
